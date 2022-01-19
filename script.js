4205d38abaa7a701a371229d33f0f433979b115b
craboudja <mon.adresse.de.zebre@gmail.com>
2022-01-19 15:07:59 +0100

    auto commit

--- script.js
+++ script.js
@@ -5,24 +5,27 @@ const mots = ['reine', 'lune', 'boule de Noël', 'maison', 'princesse', 'chien',
   'crabe', 'chat', 'échelle', 'loup', 'lapin', 'pierre', 'arbre', 'rhinocéros',
   'buisson', 'hérisson', 'escargot', 'limace', 'jardin', 'télévision', 'armoire',
   'tiroir', 'chaussure', 'chaussettes', 'souris', 'pull', 'jupe', 'roi', 'chemin',
-  'cheval', 'chevalier', 'épée', 'pomme', 'poire', 'banane', 'poupée', 'valise']
+  'cheval', 'chevalier', 'épée', 'pomme', 'poire', 'banane', 'poupée', 'valise',
+  'louche', 'fourchette', 'couteau', 'cuiller', 'clé', 'bol', 'tasse', 'verre']
 
-const sorted = mots.sort()
+// const sorted = mots.sort()
 
 const randomizeWord = words => {
-    const randomize = Math.floor(Math.random() * words.length + 1)
-    return words[randomize]
+  const randomize = Math.floor(Math.random() * words.length + 1)
+  return words[randomize]
 }
 
 const wordDisplayDiv = document.querySelector('.word')
 const wordButton = document.querySelector('.word-button')
 
+const enableButton = setInterval(() => {
+    wordDisplayDiv.innerHTML = ''
+    wordButton.disabled = false
+  }, 5000)
 
-wordButton.addEventListener('click', function() {
+const wordDisplay = () => {
     wordButton.disabled = true
     wordDisplayDiv.innerHTML += randomizeWord(mots)
-    setInterval(() => {
-        wordDisplayDiv.innerHTML = ''
-        wordButton.disabled = false
-    }, 30000)
-})
+    enableButton()
+    clearInterval(enableButton())
+}
